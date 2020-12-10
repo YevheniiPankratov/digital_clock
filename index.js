59a9be08434c4826b7b9d547c4aba1ee55b0cfd1
YevheniiPankratov <yevhenii.pankratov@nure.ua>
2020-12-10 13:19:42 +0200

    create mini-project

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
+function renderTime() {
+    let day = document.querySelector('.day'),
+        hh = document.querySelector('.hh'),
+        mm = document.querySelector('.mm'),
+        ss = document.querySelector('.ss');
 
+    let date = new Date();
+    
+    let realDay = date.getDay(),
+        realH = date.getHours(),
+        realM = date.getMinutes(),
+        realS = date.getSeconds();
 
+    checkTime(realH, hh);
+    checkTime(realM, mm);
+    checkTime(realS, ss);
 
-
-function time() {
-let day = document.querySelector('.day'),
-hh = document.querySelector('.hh'),
-mm = document.querySelector('.mm'),
-ss = document.querySelector('.ss');
-
-
-
-
-let date = new Date();
-let realDay = date.getDay();
-let realH = date.getHours();
-let realM = date.getMinutes();
-let realS = date.getSeconds();
-
-
-day.textContent = realDay;
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    day.textContent = days[realDay];
 }
 
-time();
+function checkTime(time, selector) {
+    if (time > 9) {
+        selector.textContent = time;
+    } else {
+        selector.textContent = '0' + time;
+    }
+}
+renderTime()
+setInterval(renderTime, 1000)
